bba46974d4b04a482590d6200d911dbdc20bb559
Mike Garavani <scotchmagico@gmail.com>
2024-01-29 22:01:11 +0100

    add row and column score

--- SouthPasadena/SouthPasadena.java
+++ SouthPasadena/SouthPasadena.java
@@ -3,6 +3,8 @@ package connectx.SouthPasadena;
 import java.util.Random;
 
 import connectx.CXBoard;
+import connectx.CXCell;
+import connectx.CXCellState;
 import connectx.CXGameState;
 import connectx.CXPlayer;
 
@@ -10,12 +12,23 @@ public class SouthPasadena implements CXPlayer {
 
     private Random rand;
 
+    // CONSTANTS
+
+    // Constant used in the row heuristic score evaluation
+    public static final int MULTIPLIER = 10;
+
+    // Constant used in the column heuristic score evaluation
+    public static final int MULTIPLIER_2 = 6;
+
     private int rowsNumber;
     private int columnsNumber;
     private int tokensToConnect;
 
     private CXGameState myWin;
 	private CXGameState yourWin;
+
+    private CXCellState myCell;
+    private CXCellState yourCell;
 
     /* Default empty constructor */
 	public SouthPasadena() {
@@ -31,6 +44,10 @@ public class SouthPasadena implements CXPlayer {
 
         myWin   = first ? CXGameState.WINP1 : CXGameState.WINP2;
 		yourWin = first ? CXGameState.WINP2 : CXGameState.WINP1;
+
+        myCell = first ? CXCellState.P1 : CXCellState.P2;
+        yourCell = first ? CXCellState.P2 : CXCellState.P1;
+
     }
 
     public int selectColumn(CXBoard B){
@@ -60,16 +77,125 @@ public class SouthPasadena implements CXPlayer {
         }
     }
 
+    /**
+     * Function called by heuristicScore if the current state of the game is not terminal
+     * <p>
+     * 
+     * Note:
+     * By entering this function, we know that the current state of the game is non-terminal,
+     * and therefore we can assume that there won't be any instances on the board where a player
+     * has adjacent tokens in a quantity equal to or exceeding the number required for a win
+     * ('tokensToConnect').
+     * 
+     * @param B - CXBoard object representing the current state of the game
+     * 
+     * @return The heuristic score in the current state of the game
+     * 
+     */
     private int nonTerminalHeuristicScore(CXBoard B){
-        
+
         // Initializing the heuristic score
         int score = 0;
 
-        // Initializing additional parameters
-        int iter = 0;
-
-        // Checking the row evaluation
+        // ROW EVAL
         for (int i=0; i<rowsNumber; i++){
+
+            int iter = 0;
+            int myTotalCells = 0;
+            int yourTotalCells = 0;
+
+            // If the row we're considering is completely filled, we can skip it (continue command).
+            int nonEmptyCells = 0;
+            for (int k=0; k<columnsNumber; k++){
+                if (B.cellState(i, k) != CXCellState.FREE){
+                    nonEmptyCells++;
+                }
+            }
+            if (nonEmptyCells == columnsNumber){
+                iter = 0;
+                continue;
+            }
+
+            while (iter + tokensToConnect <= columnsNumber){
+                for (int j=0; j<tokensToConnect; j++){
+                    if (B.cellState(i, j+iter) == myCell){
+                        myTotalCells++;
+                    }
+                    else if (B.cellState(i, j+iter) == yourCell){
+                        yourTotalCells++;
+                    }
+                    // Nothing happens if the cell is empty
+                }
+                if (myTotalCells == 0 && yourTotalCells > 0){
+                    score = score - yourTotalCells/tokensToConnect * MULTIPLIER;
+                }
+                else if (myTotalCells > 0 && yourTotalCells == 0){
+                    score = score + myTotalCells/tokensToConnect * MULTIPLIER;
+                }
+                myTotalCells = 0;
+                yourTotalCells = 0;
+                iter++;
+            }
+        }
+
+        // COLUMN EVAL
+        for (int j=0; j<columnsNumber; j++){
+
+            // Variable that iterates the current column, top to bottom
+            int topDownIter = rowsNumber - 1;
+
+            // Counts how many tokens from the same player there are at the top of the column
+            int count = 0;
+
+            // Establishes if the top tokens of the column are SouthPasadena's or the opponent's
+            boolean isMyColumn = true;
+
+            // Marks the first token top to bottom that is the other player's, stopping the loop
+            boolean change = false;
+            
+
+            // Going down the i-th column with 'topDownIter' until a cell is not empty
+            while (topDownIter >= 0 && (B.cellState(topDownIter, j) == CXCellState.FREE)){
+                topDownIter--;
+            }
+
+            while (topDownIter >= 0 && change == false){
+
+                // First non empty cell found
+                if (count == 0){
+                    count++;
+                    if (B.cellState(topDownIter, j) == myCell){
+                        isMyColumn = true;
+                    }
+                    else{
+                        isMyColumn = false;
+                    }
+                }
+
+                // The cell has the same color as the cells above it
+                else if (count > 0 && isMyColumn == true && B.cellState(topDownIter, j) == myCell){
+                    count++;
+                }
+
+                // The cell has the same color as the cells above it
+                else if (count > 0 && isMyColumn == false && B.cellState(topDownIter, j) == yourCell){
+                    count++;
+                }
+
+                // The cell has a different color than the cell above it
+                else{
+                    change = true;
+                }
+
+                topDownIter--;
+            }
+
+            if (count > 0 && isMyColumn){
+                score = score + count/tokensToConnect * MULTIPLIER_2;
+            }
+            else if (count > 0 && !isMyColumn){
+                score = score - count/tokensToConnect * MULTIPLIER_2;
+            }
 
         }
 
